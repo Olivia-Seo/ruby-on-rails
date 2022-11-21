50b6155298d9d127a5514f9e74d783c67f7d428d
Olivia-Seo <oliviaseo7799@gmail.com>
2022-11-21 20:55:05 +0700

    some text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
 def index
     @users = User.all.order(created_at: :desc )
+end
+
+def create
+  # Create the user from params
+  @user = User.new(user_params)
+  if @user.save
+    # Deliver the signup email
+    UserNotifierMailer.send_signup_email(@user).deliver
+    redirect_to(@user, :notice => 'User created')
+  else
+    render :action => 'new'
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :login)
+end
 end
